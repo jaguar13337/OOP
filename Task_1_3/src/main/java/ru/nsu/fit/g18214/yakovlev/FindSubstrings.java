877c6663b005e723e29100262517d322e45ae5dc
Yakovlev <a.yakovlev@g.nsu.ru>
2019-09-26 13:52:46 +0700

    Updated task 1_3.
    Reading from file + added buffer

--- Task_1_3/src/main/java/ru/nsu/fit/g18214/yakovlev/FindSubstrings.java
+++ Task_1_3/src/main/java/ru/nsu/fit/g18214/yakovlev/FindSubstrings.java
@@ -1,36 +1,49 @@
 package ru.nsu.fit.g18214.yakovlev;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 import java.util.Arrays;
+import java.util.Scanner;
 import java.util.Vector;
 
 public class FindSubstrings {
     /**
      * Find all occurrences of string 'find' in the text 'text'.
-     * @param text text, where you want to find all occurrences of String 'find'.
+     * @param fileName, where you want to find all occurrences of String 'find'.
      * @param find a substring that you want to find
      * @return
      */
 
-    public static int[] findSubstrings(String text, String find) {
+    public static int[] findSubstrings(String fileName, String find) throws IOException{
+        char[] text = new char[find.length() * 2];
         Vector<Integer> ans = new Vector<Integer>(0);
-        //File file = new File(fileName);
-        //BufferedReader br = new BufferedReader(new FileReader(file));
-        //br.close();
-        String concat = find + "$" + text;
-        int l = concat.length();
-        int Z[] = new int[l];
-        getZ(concat, Z);
-        for(int i = 0; i < l; ++i){
-            if(Z[i] == find.length()){
-                ans.add(i - find.length() - 1);
+        File file = new File(fileName);
+        BufferedReader br = null;
+        br = new BufferedReader(new FileReader(file));
+        int len = 0;
+        len = br.read(text, 0, find.length() * 2);
+        int offset = 0;
+        while (len != -1) {
+            String concat = find + "$" + (new String(text));
+            int l = concat.length();
+            int Z[] = new int[l];
+            getZ(concat, Z);
+            for (int i = 0; i < l; ++i) {
+                if (Z[i] == find.length()) {
+                    ans.add(i - find.length() - 1 + offset);
+                    for (int j = i - find.length() - 1; j < i - 1; j++)
+                        text[j] = '$';
+                }
             }
+            for (int i = 0; i < find.length(); i++)
+                text[i] = text[i + find.length()];
+            len = br.read(text, find.length(), find.length());
+            if (len < find.length())
+                for (int i = find.length() + len; i < find.length()*2; i++)
+                    text[i] = '$';
+            offset += find.length();
         }
         int[] array = new int[ans.size()];
-        for (int i = 0; i<ans.size(); i++)
+        for (int i = 0; i < ans.size(); i++)
             array[i] = ans.get(i);
         return array;
     }
--- Task_1_3/src/main/java/ru/nsu/fit/g18214/yakovlev/Main.java
+++
@@ -1,4 +0,0 @@
-package ru.nsu.fit.g18214.yakovlev;
-
-public class Main {
-}
