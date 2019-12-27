7465b25403061bf86460a6ff0560e54b3bebe739
Yakovlev <a.yakovlev@g.nsu.ru>
2019-12-27 16:50:03 +0700

    new tests

--- Task_3_2/src/test/java/ru/nsu/fit/g18214/yakovlev/GradebookListTest.java
+++ Task_3_2/src/test/java/ru/nsu/fit/g18214/yakovlev/GradebookListTest.java
@@ -7,7 +7,40 @@ import org.junit.Test;
 public class GradebookListTest {
 
   @Test
-  public void test() {
+  public void increasedScholarship() {
+    ArrayList<Grade[]> exams = new ArrayList<>();
+    Grade[] f1 = new Grade[7];
+    f1[0] = new GradeFive("Math analysis", 5);
+    f1[1] = new GradeFive("Imperative programming", 5);
+    f1[2] = new GradeFive("Declarative programming", 5);
+    f1[3] = new GradeFive("Discrete math", 5);
+    f1[4] = new Credit("English", true);
+    f1[5] = new GradeFive("History", 5);
+    f1[6] = new Credit("Sport", true);
+    exams.add(f1);
+    GradebookList gradebook = new GradebookList(exams);
+    Assert.assertTrue(gradebook.isStudentGetIncreasedScholarship());
+  }
+
+  @Test
+  public void noIncreasedScholarship() {
+    ArrayList<Grade[]> exams = new ArrayList<>();
+    Grade[] f2 = new Grade[8];
+    f2[0] = new GradeFive("Math analysis", 4);
+    f2[1] = new GradeFive("Imperative programming", 5);
+    f2[2] = new GradeFive("Declarative programming", 5);
+    f2[3] = new GradeFive("Graph theory", 5);
+    f2[4] = new GradeFive("English", 4);
+    f2[5] = new GradeFive("Russian",5);
+    f2[6] = new Credit("Sport", true);
+    f2[7] = new GraduateWork("ОСИ", 5);
+    exams.add(f2);
+    GradebookList gradebook = new GradebookList(exams);
+    Assert.assertFalse(gradebook.isStudentGetIncreasedScholarship());
+  }
+
+  @Test
+  public void meanGradeTest() {
     ArrayList<Grade[]> exams = new ArrayList<>();
     Grade[] f1 = new Grade[7];
     f1[0] = new GradeFive("Math analysis", 5);
@@ -29,9 +62,39 @@ public class GradebookListTest {
     f2[7] = new GraduateWork("ОСИ", 5);
     exams.add(f2);
     GradebookList gradebook = new GradebookList(exams);
-    Assert.assertFalse(gradebook.isStudentGetIncreasedScholarship());
-    Assert.assertTrue(gradebook.canStudentGetRedDiploma());
     Assert.assertEquals(4.82, gradebook.getAverageScore(), 0.01);
+  }
+
+  @Test
+  public void canBeRedDiploma() {
+    ArrayList<Grade[]> exams = new ArrayList<>();
+    Grade[] f2 = new Grade[8];
+    f2[0] = new GradeFive("Math analysis", 5);
+    f2[1] = new GradeFive("Imperative programming", 5);
+    f2[2] = new GradeFive("Declarative programming", 5);
+    f2[3] = new GradeFive("Graph theory", 5);
+    f2[4] = new GradeFive("English", 4);
+    f2[5] = new GradeFive("Russian",5);
+    f2[6] = new Credit("Sport", true);
+    f2[7] = new GraduateWork("ОСИ", 5);
+    exams.add(f2);
+    GradebookList gradebook = new GradebookList(exams);
+    Assert.assertTrue(gradebook.canStudentGetRedDiploma());
+  }
+  @Test
+  public void canNotBeRedDiploma() {
+    ArrayList<Grade[]> exams = new ArrayList<>();
+    Grade[] f2 = new Grade[8];
+    f2[0] = new GradeFive("Math analysis", 4);
+    f2[1] = new GradeFive("Imperative programming", 5);
+    f2[2] = new GradeFive("Declarative programming", 5);
+    f2[3] = new GradeFive("Graph theory", 5);
+    f2[4] = new GradeFive("English", 4);
+    f2[5] = new GradeFive("Russian",5);
+    f2[6] = new Credit("Sport", true);
+    f2[7] = new GraduateWork("ОСИ", 5);
+    exams.add(f2);
+    GradebookList gradebook = new GradebookList(exams);
     Grade[] f3 = new Grade[6];
     f3[0] = new GradeFive("Math analysis", 4);
     f3[1] = new GradeFive("Imperative programming", 4);
@@ -40,8 +103,6 @@ public class GradebookListTest {
     f3[4] = new GradeFive("English", 4);
     f3[5] = new GradeFive("Russian", 4);
     gradebook.addSession(f3);
-    Assert.assertFalse(gradebook.isStudentGetIncreasedScholarship());
     Assert.assertFalse(gradebook.canStudentGetRedDiploma());
-    Assert.assertEquals(4.53, gradebook.getAverageScore(), 0.01);
   }
 }
